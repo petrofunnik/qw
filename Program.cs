d44d040df876b05d77eba20b685288b97902a78a
petro <petrofunnik@gmail.com>
2023-09-24 13:27:18 +0300

    Выполнили четвертую задачу

--- Program.cs
+++ Program.cs
@@ -67,14 +67,37 @@
 
 // ----------------------------------------------
 
+// System.Console.WriteLine("Введите число: ");
+// int a = Convert.ToInt32(Console.ReadLine());
+
+// if (a%2 == 0)
+// {
+//     Console.WriteLine("да");
+// }
+// else
+// {
+//     Console.WriteLine("нет");
+// }
+
+// ------------------------------------------------
+
+
+// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+// -------------------------------------------------
+
+
 System.Console.WriteLine("Введите число: ");
 int a = Convert.ToInt32(Console.ReadLine());
-
-if (a%2 == 0)
+int b = 0;
+while (b<a)
 {
-    Console.WriteLine("да");
-}
-else
-{
-    Console.WriteLine("нет");
+    b = b +2;
+    if (b>a)
+    {
+        break;
+    }
+    Console.WriteLine(b);
 }
